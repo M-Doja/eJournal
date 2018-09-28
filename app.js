7cdf0e9d631984c23d8da592c9f985195ece562c
M-Doja <micdoja@yahoo.com>
2018-09-28 09:53:32 -0700

    replaced paypal with stripe in working order

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+const keyPublish = 'pk_test_mqsHIcQhk7MQsVdWM21RbvOp';
+const keySecret = 'sk_test_MH29dqvmnJeafJIZPiJ1SmCt';
 var express = require('express');
 var path = require('path');
+const stripe = require('stripe')(keySecret);
+const router = express.Router();
 var mongoose = require('mongoose');
 var passport = require('passport');
 var bodyParser = require('body-parser');
@@ -45,6 +49,52 @@ var entryRoutes = require('./routes/entry');
 
 app.use('/', mainRoutes);
 app.use('/entry', entryRoutes);
+
+
+app.post('/stripe/charge/thirty', (req, res) => {
+  let token = req.body.stripeToken
+  console.assert(token)
+  const amount = 399
+  stripe.charges.create({
+    amount: amount,
+    currency: 'usd',
+    source: token,
+    description: 'Post Purchase'
+  }, (err, charge) => {
+    if (err) {
+      res.redirect('/stripe/payment-failure?err_msg=' + err.message)
+    } else {
+      console.log('Charged successful')
+			console.log('charge', charge)
+      res.redirect('/payment-success')
+    }
+  })
+});
+
+app.post('/stripe/charge/ninety', (req, res) => {
+  let token = req.body.stripeToken
+  console.assert(token)
+  const amount = 999
+  stripe.charges.create({
+    amount: amount,
+    currency: 'usd',
+    source: token,
+    description: 'Post Purchase'
+  }, (err, charge) => {
+    if (err) {
+      res.redirect('/stripe/payment-failure?err_msg=' + err.message)
+    } else {
+      console.log('Charged successful')
+			console.log('charge', charge)
+      res.redirect('/payment-success')
+    }
+  })
+});
+
+app.get('/payment-success', (req, res, next) => {
+  res.render('payment-success');
+});
+
 
 app.listen(5000,() =>{
   console.log('Now running...');
